f78c4715025add63c0cbbd0bea3788bc80e3b8c3
Bahar <fatemehkhosravi.002@gmail.com>
2025-09-09 17:31:01 +0330

    create Benefactor model

--- charities/models.py
+++ charities/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-
+from accounts.models import User
 
 class Benefactor(models.Model):
-    pass
-
+    user = models.OneToOneField(User)
+    experience = models.SmallIntegerField(choices=(
+        (0 , 'mobtadi'),
+        (1 , 'motevaset'),
+        (2 , 'motekhases')
+    ) , default=0)
+    free_time_per_week = models.PositiveSmallIntegerField(default=0)
 
 class Charity(models.Model):
     pass
