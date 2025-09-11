1be65e097fdf50a10d0fcefbe6042463d62ad6af
Bahar <fatemehkhosravi.002@gmail.com>
2025-09-11 17:04:36 +0330

    costumize admin panel for accounts.admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 class User(AbstractUser):
-    address = models.TextField(blank=True , null=True)
-    age = models.PositiveSmallIntegerField(blank=True, null=True)
-    description = models.TextField(blank=True , null=True)
-    gender = models.CharField(max_length=1 , choices=(
-        ('M' , 'M'),
-        ('F' , 'F')
-    ))
-    phone = models.CharField(max_length=15 , null=True , blank=True)
+
+    gender_choices=(
+        ('F', 'Female'),
+        ('M', 'Male'),
+    )
+    email=models.EmailField(null=True, blank=True)
+    first_name=models.CharField(max_length=150 , blank=True, null=True)
+    address=models.TextField(blank=True , null=True)
+    age=models.PositiveSmallIntegerField(blank=True , null=True)
+    description=models.TextField(blank=True , null=True)
+    gender=models.CharField(max_length=1 , choices=gender_choices , blank=True ,null=True)
+    is_active=models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    phone=models.CharField(max_length=15 , blank=True , null=True)
+
+
+    
--- charities/models.py
+++ charities/models.py
@@ -1,37 +1,43 @@
 from django.db import models
-from accounts.models import User
+from django.conf import settings
 
 class Benefactor(models.Model):
-    user = models.OneToOneField(User)
-    experience = models.SmallIntegerField(choices=(
-        (0 , 'mobtadi'),
-        (1 , 'motevaset'),
-        (2 , 'motekhases')
-    ) , default=0)
-    free_time_per_week = models.PositiveSmallIntegerField(default=0)
+    experience_choices=(
+        (0,'مبتدی'),
+        (1,'متوسط'),
+        (2,'متخصص'),
+    )
+
+    user=models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    experience=models.SmallIntegerField(choices=experience_choices , default=0)
+    free_time_per_week=models.PositiveSmallIntegerField(default=0)
+
 
 class Charity(models.Model):
-    user = models.OneToOneField(User)
-    name = models.CharField(max_length=50)
-    reg_number = models.CharField(max_length=10)
-    
+    user=models.OneToOneField(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
+    name=models.CharField(null=True , blank=True , max_length=50)
+    reg_number=models.CharField(max_length=10)
 
 
 class Task(models.Model):
-    assigned_benefactor = models.ForeignKey(Benefactor , null=True , on_delete=models.SET_NULL)
-    charity = models.ForeignKey(Charity)
-    age_limit_from = models.IntegerField()
-    age_limit_to = models.IntegerField()
-    date = models.DateField(null=True , blank=True)
-    description = models.TextField()
-    gender_limit = models.CharField(max_length=1 , blank=True , null=True , choices=(
-        ('M' , 'M'),
-        ('F' , 'F')
-    ))
-    state = models.CharField(max_length=1 , choices=(
-        ('A' , 'Assigned'),
-        ('W' , 'Waiting'),
+    gender_choices=(
+        ('F', 'Female'),
+        ('M', 'Male'),
+    )
+
+    state_choices=(
         ('P' , 'Pending'),
-        ('D' , 'Done')
-    ) , default='P')
-    title = models.CharField(max_length=60)
+        ('W' , 'Waiting'),
+        ('A' , 'Assigned'),
+        ('D', 'Done'),
+
+    )
+    assigned_benefactor=models.ForeignKey(Benefactor ,blank=True, null=True ,  on_delete=models.SET_NULL)
+    charity=models.ForeignKey(Charity , on_delete=models.CASCADE)
+    age_limit_from=models.IntegerField(blank=True , null=True)
+    age_limit_to=models.IntegerField(blank=True , null=True)
+    date=models.DateField(blank=True , null=True)
+    description=models.TextField(blank=True , null=True)
+    gender_limit=models.CharField(max_length=1 , choices=gender_choices , blank=True , null=True)
+    state=models.CharField(max_length=1 , choices=state_choices , default='P')
+    title=models.CharField(max_length=60)
